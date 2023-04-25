9518d7a7d49cb22d12fc8d7f1aaee4a0c425279f
Dan Klausner <dan@melio.com>
2023-04-25 11:25:57 +0300

    exercise 2 solution

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -3,19 +3,29 @@ import React from 'react';
 import Button from '../Button';
 
 import styles from './ToastPlayground.module.css';
+import Toast from '../Toast/Toast';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
   const [message, setMessage] = React.useState('');
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [isToastOpen, setIsToastOpen] = React.useState(false);
   return (
     <div className={styles.wrapper}>
       <header>
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
-
+      {isToastOpen && (
+        <Toast
+          message={message}
+          variant={variant}
+          onClose={() => {
+            setIsToastOpen(false);
+          }}
+        />
+      )}
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
           <label
@@ -61,7 +71,13 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button
+              onClick={() => {
+                setIsToastOpen(true);
+              }}
+            >
+              Pop Toast!
+            </Button>
           </div>
         </div>
       </div>
